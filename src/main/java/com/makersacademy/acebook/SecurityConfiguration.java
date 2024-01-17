0d7133f144d8025bef1b8b4ec6d5aab725cc56a3
Callum Bain <callumjbain@gmail.com>
2024-01-17 15:36:31 +0000

    custom login page added, no styling

--- src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/SecurityConfiguration.java
@@ -28,7 +28,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/posts").hasRole("USER")
                 .antMatchers("/users").permitAll()
-                .and().formLogin();
+                .and()
+                .formLogin()
+                .loginPage("/user-login") //this is the default login page when visiting 8080...
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/posts") //when login is successful this is new default url
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout") //where to default to when logout function is run
+                .logoutSuccessUrl("/user-login?logout") //when you click logout this is where you are directed
+                .permitAll();
+
     }
 
     @Bean
